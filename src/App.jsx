dcf53cafd41d8a6b44337d0299ae79cd71534d6d
Malaqmal <maldev.developer@gmail.com>
2026-07-20 21:27:04 +0700

    update branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+const ajhsgv =87654;
+
 function App() {
   const [count, setCount] = useState(0)
 
